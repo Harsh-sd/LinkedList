008fc609e209cc3d8401519d038a5e7413378b59
Harsh-sd <harshsandilya05042002@gmail.com>
2024-04-09 22:37:13 +0530

    inserting at head and tail in singly linked list

---
+++ inserttail.cpp
@@ -0,0 +1,78 @@
+
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+void insertathead(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    n->next = head;
+    head = n;
+}
+void insertattail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+// we can take the data typoe as bool and int as well
+// when we take bool then we have to return true or false
+int search(node *head, int key)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            return temp->data;
+        }
+        temp = temp->next;
+    }
+    return -1;
+}
+int main()
+{
+    node *head = NULL;
+    insertattail(head, 1);
+    insertattail(head, 2);
+    insertattail(head, 3);
+    insertattail(head, 4);
+    display(head);
+    insertathead(head, 5);
+    display(head);
+    cout << search(head, 5);
+    return 0;
+}
