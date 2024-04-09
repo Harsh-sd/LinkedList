843f183bc188011fdda718df66fa3a33d5c9c69e
Harsh-sd <harshsandilya05042002@gmail.com>
2024-04-09 23:12:10 +0530

    reversing k nodes

---
+++ reverseknodes.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertattail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+node *reverse(node *&head, int k)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+
+    // initializing count as 0
+    int count = 0;
+    while (currptr != NULL && count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+    // recusive way
+    if (nextptr != NULL)
+    {
+        // calling for next head
+        head->next = reverse(nextptr, k);
+    }
+    return prevptr;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertattail(head, 1);
+    insertattail(head, 2);
+    insertattail(head, 3);
+    insertattail(head, 4);
+    insertattail(head, 5);
+    insertattail(head, 6);
+    display(head);
+    node *newhead = reverse(head, 2);
+    display(newhead);
+
+    return 0;
+}
