adb0b68ad7821682724d3536a5c3eb8f557cc024
Harsh-sd <harshsandilya05042002@gmail.com>
2024-04-10 10:36:49 +0530

    deletion indouble linked list

---
+++ deletionindoubly.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+}
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+void deleteAtHead(node *&head)
+{
+    node *todelete = head;
+    head = head->next;
+    head->prev = NULL;
+    delete todelete;
+}
+void deletion(node *&head, int pos)
+{
+    node *temp = head;
+    int count = 1;
+    while (temp != NULL && count != pos)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    temp->prev->next = temp->next;
+    if (temp->next != NULL)
+    {
+
+        temp->next = temp->next->prev;
+    }
+    delete temp;
+}
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    display(head);
+    deletion(head, 3);
+    display(head);
+    deleteAtHead(head);
+    display(head);
+}
